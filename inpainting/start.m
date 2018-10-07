3d90c02ad2be143fa26bcefe705b1d2976a2cfde
Jonatan <jonatan@adsmurai.com>
2018-10-07 18:32:58 +0200

    style: add spaces and fix identations to improve readability

--- inpainting/start.m
+++ inpainting/start.m
@@ -1,51 +1,56 @@
-%Example script: You should replace the beginning of each function ('sol')
-%with the name of your group. i.e. if your gropu name is 'G8' you should
-%call :
+% Example script: You should replace the beginning of each function ('sol')
+% with the name of your group. i.e. if your gropu name is 'G8' you should
+% call :
 % G8_DualTV_Inpainting_GD(I, mask, paramInp, paramROF)
 
 clearvars;
-%There are several black and white images to test:
-%  image1_toRestore.jpg
-%  image2_toRestore.jpg
-%  image3_toRestore.jpg
-%  image4_toRestore.jpg
-%  image5_toRestore.jpg
+% There are several black and white images to test:
+% image1_toRestore.jpg
+% image2_toRestore.jpg
+% image3_toRestore.jpg
+% image4_toRestore.jpg
+% image5_toRestore.jpg
 
-%name= 'image5';
-name= 'image1';
+% name = 'image5';
+name = 'image1';
 
 I = double(imread([ name '_toRestore.jpg']));
-%I=I(1:10,1:10);
+% I = I(1:10,1:10);
 
-%Number of pixels for each dimension, and number of channles
+% Number of pixels for each dimension, and number of channles
 [ni, nj, nC] = size(I);
 
-if nC==3
-    I = mean(I,3); %Convert to b/w. If you load a color image you should comment this line
+if nC == 3
+  % Convert to b/w. If you load a color image you should comment this line
+  I = mean(I,3); 
 end
 
-%Normalize values into [0,1]
-I=I-min(I(:));
-I=I/max(I(:));
+% Normalize values into [0,1]
+I = I-min(I(:));
+I = I/max(I(:));
 
-%Load the mask
+% Load the mask
 mask_img = double(imread([name '_mask.jpg']));
-%mask_img =mask_img(1:10,1:10);
+%mask_img = mask_img(1:10,1:10);
+
 [ni, nj, nC] = size(mask_img);
-if nC==3
-    mask_img = mask_img(:,:,1); %Convert to b/w. If you load a color image you should comment this line
+
+if nC == 3
+  % Convert to b/w. If you load a color image you should comment this line
+  mask_img = mask_img(:,:,1); 
 end
-%We want to inpaint those areas in which mask == 1
-mask = mask_img >128; %mask(i,j) == 1 means we have lost information in that pixel
-                      %mask(i,j) == 0 means we have information in that
-                      %pixel
-                                                                    
-%%%Parameters for gradient descent (you do not need for week1)
+
+% We want to inpaint those areas in which mask == 1
+mask = mask_img > 128; 
+% mask(i,j) == 1 means we have lost information in that pixel
+% mask(i,j) == 0 means we have information in that pixel
+ 
+%%% Parameters for gradient descent (you do not need for week1)
 param.dt = 5*10^-7;
 param.iterMax = 10^4;
 param.tol = 10^-5;
 
-%%Parameters 
+%% Parameters 
 param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
 
@@ -61,18 +66,18 @@ title('After');
 
 %% Challenge image. (We have lost 99% of information)
 clearvars
-I=double(imread('image6_toRestore.jpg'));
-%Normalize values into [0,1]
-I=I/256;
+I = double(imread('image6_toRestore.jpg'));
+% Normalize values into [0,1]
+I = I / 256;
 
 
-%Number of pixels for each dimension, and number of channels
+% Number of pixels for each dimension, and number of channels
 [ni, nj, nC] = size(I);
 
-mask_img=double(imread('image6_mask.jpg'));
-mask = mask_img >128; %mask(i,j) == 1 means we have lost information in that pixel
-                      %mask(i,j) == 0 means we have information in that
-                      %pixel
+mask_img = double(imread('image6_mask.jpg'));
+mask = mask_img > 128; 
+% mask(i,j) == 1 means we have lost information in that pixel
+% mask(i,j) == 0 means we have information in that pixel
 
 param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
@@ -92,7 +97,7 @@ title('After');
 %% Goal Image
 clearvars;
 
-%Read the image
+% Read the image
 I = double(imread('image_to_Restore.png'));
 
 [ni, nj, nC] = size(I);
@@ -101,23 +106,23 @@ I = double(imread('image_to_Restore.png'));
 I = I - min(I(:));
 I = I / max(I(:));
 
-%We want to inpaint those areas in which mask == 1 (red part of the image)
+% We want to inpaint those areas in which mask == 1 (red part of the image)
 I_ch1 = I(:,:,1);
 I_ch2 = I(:,:,2);
 I_ch3 = I(:,:,3);
 
-%TO COMPLETE 1
-%mask_img(i,j) == 1 means we have lost information in that pixel
-%mask(i,j) == 0 means we have information in that pixel
+% TO COMPLETE 1
+% mask_img(i,j) == 1 means we have lost information in that pixel
+% mask(i,j) == 0 means we have information in that pixel
 % Red is found in a normalized RGB colour channel as (1,0,0)
 mask = (I_ch1 == 1) & (I_ch2 == 0) & (I_ch3 == 0);
 
-%%%Parameters for gradient descent (you do not need for week1)
-%param.dt = 5*10^-7;
-%param.iterMax = 10^4;
-%param.tol = 10^-5;
+%%% Parameters for gradient descent (you do not need for week1)
+% param.dt = 5*10^-7;
+% param.iterMax = 10^4;
+% param.tol = 10^-5;
 
-%parameters
+% parameters
 param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
 
