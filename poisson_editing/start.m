c399d3227a8c27faed113e64caf32842f7d15aa4
Jonatan <jonatan@adsmurai.com>
2018-10-15 16:33:41 +0200

    refactor: avoid re-computing gradients
    Combine eye and mouth masks into one to compute gradients once.

--- poisson_editing/start.m
+++ poisson_editing/start.m
@@ -11,10 +11,13 @@ param.hj = 1;
 %param.method = 'importing';
 param.method = 'mixing';
 
-%masks to exchange: Eyes
-mask_src = logical(imread('mask_src_eyes.png'));
-mask_dst = logical(imread('mask_dst_eyes.png'));
+% Combine all masks in one
+mask_src = logical(imread('mask_src_eyes.png')) | ...
+            logical(imread('mask_src_mouth.png'));
+mask_dst = logical(imread('mask_dst_eyes.png')) | ...
+            logical(imread('mask_dst_mouth.png'));
 
+result = zeros(size(dst));
 for nC = 1: nChannels
   [grad, driving] = do_it_for_a_channel(src(:,:,nC), dst(:,:,nC), ...
                                         mask_src, mask_dst, ...
@@ -25,18 +28,4 @@ for nC = 1: nChannels
   result(:,:,nC) = G4_Poisson_Equation_Axb(dst(:,:,nC), mask_dst, param);
 end
 
-%Mouth
-%masks to exchange: Mouth
-mask_src=logical(imread('mask_src_mouth.png'));
-mask_dst=logical(imread('mask_dst_mouth.png'));
-for nC = 1: nChannels
-  [grad, driving] = do_it_for_a_channel(src(:,:,nC), dst(:,:,nC), ...
-                                        mask_src, mask_dst, ...
-                                        param);
-
-  param.dst_grad = grad;
-  param.driving = driving;
-  result(:,:,nC) = G4_Poisson_Equation_Axb(result(:,:,nC), mask_dst, param);
-end
-
-figure, imshow(result/256)
+figure(2), imshow(result/256)
