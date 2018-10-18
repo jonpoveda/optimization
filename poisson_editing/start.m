a87d8d4d9d6abc01dbcf0e232d4614cc22e84144
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2018-10-18 20:57:43 +0200

    Added a few extra examples and the source image used

--- poisson_editing/start.m
+++ poisson_editing/start.m
@@ -2,8 +2,8 @@ clearvars;
 
 % Sets parameters
 % Seamless cloning with importing gradients or mixing gradients (optional)
-param.method = 'importing';
-%param.method = 'mixing';
+%param.method = 'importing';
+param.method = 'mixing';
 
 % Sets gradient normalization
 param.hi=1;
@@ -14,18 +14,30 @@ equalize = false;
 
 % Pick images and masks (choose one example)
 % Example 1
-src_path = 'girl.png';  % flipped girl, because of the eyes
-dst_path = 'lena.png';
-src_mask_path = 'mask_src_eyes.png';
-dst_mask_path = 'mask_dst_eyes.png';
-src2_mask_path = 'mask_src_mouth.png';
-dst2_mask_path = 'mask_dst_mouth.png';
+% src_path = 'girl.png';  % flipped girl, because of the eyes
+% dst_path = 'lena.png';
+% src_mask_path = 'mask_src_eyes.png';
+% dst_mask_path = 'mask_dst_eyes.png';
+% src2_mask_path = 'mask_src_mouth.png';
+% dst2_mask_path = 'mask_dst_mouth.png';
 
 % Example 2
 % src_path = 'stamps.png';
 % dst_path = 'passport.png';
-% src_mask_path = 'mask_src_stam.png';
+% src_mask_path = 'mask_src_stamp.png';
 % dst_mask_path = 'mask_dst_stamp.png';
+
+% Example 3
+% src_path = 'forest_moon.jpg';
+% dst_path = 'mountain_reflection.jpg';
+% src_mask_path = 'mask_src_forest_moon.png';
+% dst_mask_path = 'mask_dst_mountains_reflection.png';
+
+% Example 4
+src_path = 'children.jpg';
+dst_path = 'book_def.jpg';
+src_mask_path = 'mask_src_children.png';
+dst_mask_path = 'mask_dst_book.png';
 
 % Loads images
 dst = double(imread(dst_path));
@@ -33,8 +45,8 @@ src = double(imread(src_path));
 [ni,nj, nChannels]=size(dst);
 
 if equalize
-  dst = colour_equalize(dst);
-  src = colour_equalize(src);
+  dst = colour_equalize(uint8(dst));
+  src = colour_equalize(uint8(src));
 end
 
 % Placeholder for the result
