9449e0bb80e85ed30a26d6a97a5f0a7b7c393dc6
Jonatan <jonatan@adsmurai.com>
2018-10-28 19:32:10 +0100

    feat: fill TODO #19

--- segmentation/G4_diracReg.m
+++ segmentation/G4_diracReg.m
@@ -3,6 +3,6 @@ function y = G4_diracReg( x, epsilon )
   %    G4_diracReg( x, epsilon ) Computes the derivative of the heaviside
   %    function of x with respect to x. Regularized based on epsilon.
 
-  y = ??; %TODO 19: Line to complete
+  y = epsilon ./ (pi * ( epsilon^2 + x.^ 2)); %TODO 19: Line to complete
 
 end
