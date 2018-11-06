3a02d5328a91a9db73f4bd14f1e4cfb7f81d8d82
Jonatan <jonatan@adsmurai.com>
2018-11-06 21:14:00 +0100

    Add some logging messages

--- segmentation/start.m
+++ segmentation/start.m
@@ -7,6 +7,7 @@ experiment_name = 'example1'
 output_folder = fullfile('figures/', experiment_name);
 mkdir(output_folder);
 
+disp('Loading example');
 % Load example (each image has diff 'good' parameter values)
 [I, p] = load_example(1);
 
@@ -25,10 +26,12 @@ I = I / max(I(:));
 % p.reIni = 500;
 
 %% Runs Explicit Gradient Descent
+disp('Running gradient descent');
 seg = G4_ChanVeseIpol_GDExp(I, ...
   p.phi_0, p.mu, p.nu, p.eta, p.lambda1, p.lambda2, p.tol, ...
   p.epHeaviside, p.dt, p.iterMax, p.reIni, ...
   output_folder);
 
 % Write a GIF with output figures
+disp('Creating an animation');
 animate_images(output_folder);
