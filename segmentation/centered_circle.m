81772615991ce4b379e25caa6fddfc90639f5d39
Jonatan <jonatan@adsmurai.com>
2018-11-07 19:13:47 +0100

    doc: improve centered_circle documentation

--- segmentation/centered_circle.m
+++ segmentation/centered_circle.m
@@ -1,4 +1,5 @@
 function [phi_0] = centered_circle(X, Y, ni, nj)
-  % Default (circle centered around (ni/2, nj/2) with a radius sqrt(50))
-  phi_0 = (-sqrt( (X - round(ni/2)) .^2 + (Y - round(nj/2) ) .^2) + 50);
+  % Default (circle centered around (ni/2, nj/2) with a radius sqrt(a))
+  a = (ni+nj)/4;
+  phi_0 = (-sqrt( (X - round(ni/2)) .^2 + (Y - round(nj/2)) .^2) + a);
 end
